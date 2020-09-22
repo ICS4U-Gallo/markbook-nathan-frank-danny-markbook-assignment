6c748a9d49c9a875279b5f1e130bb8035162d6fc
Tianyu Li <44486240+GamingFrankie@users.noreply.github.com>
2020-09-22 14:53:52 -0400

    Update Draft Ideas.py

--- Draft Ideas.py
+++ Draft Ideas.py
@@ -1,1 +1,16 @@
 """Lets put what we think in here"""
+
+"""Add Student to Classroom"""
+def add_student_to_classroom(student: Dict, classroom: Dict):
+    """Adds student to a classroom
+    
+    Args:
+        student: Student dict
+        classroom: The classroom to add the student to
+    """
+    
+    # Add 
+    students = classroom["student_list"] 
+    students.append(student)    # Append the student dictionary into the student list inside the classroom dictionary
+
+    return classroom

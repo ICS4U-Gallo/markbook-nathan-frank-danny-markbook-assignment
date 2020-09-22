94da7439bebb94ec5e62e2d2e9b1536b3c96cbae
Tianyu Li <44486240+GamingFrankie@users.noreply.github.com>
2020-09-22 15:00:02 -0400

    Update Draft Ideas.py

--- Draft Ideas.py
+++ Draft Ideas.py
@@ -14,3 +14,14 @@ def add_student_to_classroom(student: Dict, classroom: Dict):
     students.append(student)    # Append the student dictionary into the student list inside the classroom dictionary
 
     return classroom
+
+"""Remove Student to Classroom"""
+def remove_student_from_classroom(student: Dict, classroom: Dict):
+    """Removes student from classroom
+    Args:
+        student: The student to be removed
+        classroom: the class from which the student will be removed.
+    """
+    students = classroom["student_list"]
+    students.remove(student)    # Based on the logic above, remove the student dictionary into the student list inside the classroom dictionary
+    

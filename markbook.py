972ca4fea857ceba258a91defd856ee59438ac49
Danny Lu <danny.lu075@gmail.com>
2020-09-24 14:14:17 -0400

    Update markbook.py

--- markbook.py
+++ markbook.py
@@ -3,35 +3,6 @@ Markbook Application
 Group members: Danny, Frank, Nathan
 """
 from typing import Dict, List
-
-classrooms = []
-
-def main():
-    while True:
-        print("Type 'classroom' if you want to create a classroom")
-        print("Type 'list' if you want to list the classrooms")
-        print("Type 'exit' if you want to exit")
-
-        try:
-            choice = input("Select a menu option:   ").lower()
-        except ValueError:
-            print("Error. Please enter a string")
-        
-        if choice == "classroom":
-            create_classroom_interface()
-        elif choice == "list":
-            list_classrooms_interface()
-        elif choice == "exit":
-            exit()
-        else:
-            print("Please enter a vaid choice")
-        
-
-def create_classroom_interface():
-    pass
-
-def list_classrooms_interface():
-    pass
     
 
 def create_assignment(name: str, due: str, points: int) -> Dict:

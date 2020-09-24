d1ab3fedfea8b232ce8cd4301d8bf868bd7c0aa1
Danny Lu <danny.lu075@gmail.com>
2020-09-24 14:13:59 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,21 +1,19 @@
-from markbook import create_assignment, create_classroom, calculate_average_mark, add_student_to_classroom, remove_student_from_classroom, edit_student
-
+from markbook import *
 from typing import Dict, List
+import json
 
 classrooms = []
 
 def main():
     while True:
-        print('Type "classroom" if you want to create a classroom')
-        print('Type "list" if you want to list the classrooms')
-        print('Type "exit" if you want to exit')
-        # Grammar fix in case anyone wondering.
-        # In a sentence, it is more proper to use " rather than ' to reference sth.
+        print("Type 'classroom' if you want to create a classroom")
+        print("Type 'list' if you want to list the classrooms")
+        print("Type 'exit' if you want to exit")
 
         try:
             choice = input("Select a menu option:   ").lower()
         except ValueError:
-            print("Error. Please enter a string")
+            print("Error, Please enter a string")
         
         if choice == "classroom":
             create_classroom_interface()
@@ -28,27 +26,32 @@ def main():
         
 
 def create_classroom_interface():
-    # This is from Frank, so there is no "glitchless-guarantee."
-    course_code = str(input("Course Code: "))
-    course_name = str(input("Course Name: "))
-    period = str(input("Period: "))
-    teacher_name = str(input("Teacher Name: "))
-    student_list = list(input("Student List: "))
-    assignments_list = list(input("Assignments: ")
-    # I gathered the informations of the course above.
-    
+    while True:
+        try:
+            course_code = input("Course Code:  ")
+            course_name = input("Course Name:  ")
+            period = input("Period:  ")
+            teacher = input("Teacher Name:  ")
+        except ValueError:
+            print("Error, please enter a string")
+
+
     classroom = {
     "course code": course_code,
     "course name": course_name, 
     "period": period,
-    "teacher name": teacher_name,
-    "student list": student_list, 
-    "assignments list": assignments_list
-    # I listed the information into a dictionary.
+    "teacher name": teacher,
     }
+
+    classroom = json.dumps(classroom)
+
+    with open("data.json", "w") as writer:
+        json.dump(classroom, writer)
+
     new_class = json.dumps(classroom)
-    # I dump the dictionary into .json file.
     return new_class
 
 def list_classrooms_interface():
     pass
+
+main()

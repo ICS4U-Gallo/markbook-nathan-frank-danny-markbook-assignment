46a609251983f9edd5a81281e806c9110d8fd4ab
Danny Lu <danny.lu075@gmail.com>
2020-09-27 11:17:30 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from functions import create_assignment_interface, create_classroom_interface, list_assignment_interface, list_classrooms_interface, add_student_to_classroom, add_to_data, main, clear
+
+if __name__ == "__main__":
+    main()

f7ae0fe81f2534072cc440e59a494177fff13656
Tianyu Li <44486240+GamingFrankie@users.noreply.github.com>
2020-10-07 14:14:28 -0400

    Update markbook_2.py

--- markbook_2.py
+++ markbook_2.py
@@ -7,12 +7,22 @@ class added:
 - class Assignments
 - class Students
 
+Changes:
+- added __init__
+
 """
 from typing import Dict 
 
-class Assignments:
+class Assignment:
+    def __init__(self):
+        name: str = None
+        due: str = None
+        points: int = None
+        self.name = name
+        self.due = due
+        self.points = points
 
-    def create_assignment(name: str, due: str, points: int) -> Dict:
+    def create_assignment(self, name: str, due: str, points: int) -> Dict:
         """Creates an assignment represented as a dictionary
     
         Args:
@@ -22,23 +32,46 @@ class Assignments:
         Returns:
             Assignment as a dictionary.
         """
-        assignment = {"name": name, 
-                  "due": due, 
-                  "points": points
-                }
+
+        self.name = name
+        self.due = due
+        self.points = points
+        assignment = {
+            "name": self.name, 
+            "due": self.due, 
+            "points": self.points
+        }
         return assignment
 
-class Students:
 
-    def calculate_average_mark(student: Dict) -> float:
+class Student:
+
+    def __init__(self, first_name: str, last_name: str, gender: str, image: str, student_number: str, grade: int, email: str, marks: list, comments: str):
+        student: Dict = {
+            "first_name": first_name, 
+            "last_name": last_name, 
+            "gender": gender, 
+            "image": image, 
+            "student_number": student_number, "grade": grade, 
+            "email": email, 
+            "marks": marks, 
+            "comments": comments
+            }
+        self.student = student
+
+
+    def calculate_average_mark(self, student: Dict) -> float:
         """Calculates the average mark of a student"""
         total_marks = 0
-        student_marks = student["marks"]
+        student_marks = self.student["marks"]
     
         for mark in student_marks:
-            total_marks += mark      
+            total_marks += mark
 
-    def edit_student(student: Dict, **kwargs: Dict):
+        average_mark = total_marks / len(student_marks)
+        return average_mark
+
+    def edit_student(self, student: Dict, **kwargs: Dict):
         """Edits the student's info with the provided key/value pairs
         Args:
             student: The student whose data needs to be udated.
@@ -46,5 +79,5 @@ class Students:
                 data that needs to be changed. Can come in the form of a dictionary.
     """
         for key, value in kwargs.items():
-            student[key] = value
-        return student
+            self.student[key] = value
+    

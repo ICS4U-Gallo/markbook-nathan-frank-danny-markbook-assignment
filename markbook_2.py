e5b2b50b91b1ecb96cb6912a67fcc3b60b058600
Tianyu Li <44486240+GamingFrankie@users.noreply.github.com>
2020-10-07 15:07:30 -0400

    Update markbook_2.py

--- markbook_2.py
+++ markbook_2.py
@@ -6,6 +6,9 @@ MANUAL
 class added:
 - class Assignments
 - class Students
+- class Classroom
+- class Teacher (working on it)
+- class Person (working on it)
 
 Changes:
 - added __init__
@@ -13,41 +16,34 @@ Changes:
 """
 from typing import Dict 
 
+
+# class Assignment
+
 class Assignment:
     
     def __init__(self):
-        name: str = None
-        due: str = None
-        points: int = None
-        self.name = name
-        self.due = due
-        self.points = points
+
+        self.assignment = None
+
 
     def create_assignment(self, name: str, due: str, points: int) -> Dict:
-        """Creates an assignment represented as a dictionary
-    
-        Args:
-            name: the name of the assignmeyo nt.
-            due: the due date for the assignment.
-            points: what the assignment is out of (denominator).
-        Returns:
-            Assignment as a dictionary.
-        """
+        """Creates an assignment represented as a dictionary"""
 
-        self.name = name
-        self.due = due
-        self.points = points
-        assignment = {
-            "name": self.name, 
-            "due": self.due, 
-            "points": self.points
+        assignment: Dict = {
+            "name": name, 
+            "due": due, 
+            "points": points
         }
-        return assignment
+        self.assignment = assignment
 
+
+# class Student
 
 class Student:
 
     def __init__(self, first_name: str, last_name: str, gender: str, image: str, student_number: str, grade: int, email: str, marks: list, comments: str):
+        """Create a student entry"""
+
         student: Dict = {
             "first_name": first_name, 
             "last_name": last_name, 
@@ -63,6 +59,7 @@ class Student:
 
     def calculate_average_mark(self, student: Dict) -> float:
         """Calculates the average mark of a student"""
+
         total_marks = 0
         student_marks = self.student["marks"]
     
@@ -71,6 +68,7 @@ class Student:
 
         average_mark = total_marks / len(student_marks)
         return average_mark
+
 
     def edit_student(self, student: Dict, **kwargs: Dict):
         """Edits the student's info with the provided key/value pairs
@@ -81,4 +79,51 @@ class Student:
     """
         for key, value in kwargs.items():
             self.student[key] = value
+            
+            
+# class Classroom
+
+class Classroom:
+
+    def __init__(self):
+        self.classroom = None
+
+
+    def create_classroom(self, course_code: str, course_name: str, period: int, teacher: str) -> Dict:
+        """Creates a classroom dictionary"""
+
+        classroom: Dict = {
+            "course_code": course_code,
+            "course_name": course_name,
+            "period": period,
+            "teacher": teacher,
+            "student_list": [],
+            "assignment_list": []
+        }
+        self.classroom = classroom
     
+
+    def add_student_to_classroom(self, student: Dict, classroom: Dict):
+        """Adds student to a classroom"""
+
+        self.classroom["student_list"].append(student)
+
+
+    def remove_student_from_classroom(self, student: Dict, classroom: Dict):
+        """Removes student from classroom"""
+
+        self.classroom["student_list"].remove(student)
+
+# class Teacher (nothing yet)
+
+class Teacher:
+    None
+    
+    
+# class Person (unfinished)
+
+class Person:
+
+    def __init__(self, person: Dict):
+        self.person = person
+        
